1e219cd8d2bebec29dcc936de774370a0459a261
Badreddine-Ab <badreddine@gmail.com>
2023-01-03 14:27:13 +0100

    adding the error handling middlewares

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 
 // Import other dependencies
 const express = require('express');
+const globalError = require('./middleware/errorMiddleware')
 
 // Import database connection
 const sequelize = require('./config/databse');
@@ -23,8 +24,19 @@ const {
 const app = express();
 
 // Set up routes, middleware, etc.
+app.use(globalError);
 
 // Start the server
 app.listen(process.env.PORT, () => {
   console.log('Server is listening on port' + process.env.PORT);
 });
+
+// Handle errors outside express
+process.on("unhandledRejection",(err)=> {
+  console.error(`UnhandledRejection Errors : ${err.name} | ${err.message}`);
+  server.close(()=> {
+      console.error('Shutting down....')
+      process.exit(1)
+  })
+  
+})
